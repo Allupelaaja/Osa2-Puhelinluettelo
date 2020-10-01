1753c6ef052001e597d0c4a2e47286e5418e3e0a
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-10-01 17:31:04 +0300

    3.11 tehty

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://pacific-hollows-40720.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
